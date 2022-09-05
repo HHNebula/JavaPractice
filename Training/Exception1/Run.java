d0a6feee183e7855313f9e1ba225de69eca84b09
SOO <111834203+HHNebula@users.noreply.github.com>
2022-09-05 18:28:44 +0900

    Create Run.java

---
+++ Training/Exception1/Run.java
@@ -0,0 +1,15 @@
+package com.kh.practice.numRange.run;
+
+import java.io.IOException;
+
+import com.kh.practice.numRange.view.NumberMenu;
+
+public class Run {
+
+	public static void main(String[] args) throws IOException {
+
+		new NumberMenu().menu();
+
+	}
+
+}
