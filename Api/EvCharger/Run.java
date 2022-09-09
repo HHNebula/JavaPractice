ccc05a5bbeceabb7a41982c6047313a8e09ccfb8
SOO <111834203+HHNebula@users.noreply.github.com>
2022-09-09 16:48:22 +0900

    Create Run.java

---
+++ Api/EvCharger/Run.java
@@ -0,0 +1,13 @@
+package data.evcharger.run;
+
+import data.evcharger.api.EvChargerApi;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		new EvChargerApi().run();
+		
+	}
+
+}
