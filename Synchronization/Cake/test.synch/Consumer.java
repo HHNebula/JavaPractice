9e5edbf7cdc03770c8657228384d9dc4f59babb0
SOO <111834203+HHNebula@users.noreply.github.com>
2022-09-27 17:58:09 +0900

    Create Consumer.java

---
+++ Synchronization/Cake/test.synch/Consumer.java
@@ -0,0 +1,23 @@
+package test.synch;
+
+import test.common.Buffer;
+
+public class Consumer implements Runnable {
+	private Buffer buffer;
+
+	public Consumer(Buffer drop) {
+		this.buffer = drop;
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			int data = buffer.get();
+			System.out.println("소비자: " + data + "번 케익을 소비하였습니다.");
+			try {
+				Thread.sleep((int) (Math.random() * 100));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
